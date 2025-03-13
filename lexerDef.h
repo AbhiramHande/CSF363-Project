909a0188e252712f42c1dba86f2038fa7edfbb53
AbhiramHande <abhiram.h2003@gmail.com>
2025-03-13 08:27:36 +0530

    Remade lexer

--- lexerDef.h
+++ lexerDef.h
@@ -1,17 +1,14 @@
 #ifndef LEXERDEF_H
 #define LEXERDEF_H
+#define BUFFER_SIZE 512
+
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct twinBuffer
-{
-    char *buffer1; // Declare buffer1 as a pointer
-    char *buffer2;
+typedef struct TwinBuffer twin_buffer;
+typedef struct Token token;
 
-} twinBuffer;
-
-typedef enum
-{
+typedef enum {
     TK_ASSIGNOP,
     TK_COMMENT,
     TK_FIELDID,
@@ -26,6 +23,23 @@ typedef enum
     TK_WHILE,
     TK_UNION,
     TK_ENDUNION,
+    TK_SQL,
+    TK_SQR,
+    TK_COMMA,
+    TK_SEM,
+    TK_COLON,
+    TK_DOT,
+    TK_OP,
+    TK_CL,
+    TK_PLUS,
+    TK_MINUS,
+    TK_MUL,
+    TK_DIV,
+    TK_AND,
+    TK_OR,
+    TK_NOT,
+    TK_NE,
+    TK_EQ,
     TK_DEFINETYPE,
     TK_AS,
     TK_TYPE,
@@ -33,60 +47,49 @@ typedef enum
     TK_GLOBAL,
     TK_PARAMETER,
     TK_LIST,
-    TK_SQL,
-    TK_SQR,
     TK_INPUT,
     TK_OUTPUT,
     TK_INT,
     TK_REAL,
-    TK_COMMA,
-    TK_SEM,
-    TK_COLON,
-    TK_DOT,
     TK_ENDWHILE,
-    TK_OP,
-    TK_CL,
     TK_IF,
     TK_THEN,
     TK_ENDIF,
     TK_READ,
     TK_WRITE,
     TK_RETURN,
-    TK_PLUS,
-    TK_MINUS,
-    TK_MUL,
-    TK_DIV,
     TK_CALL,
     TK_RECORD,
     TK_ENDRECORD,
     TK_ELSE,
-    TK_AND,
-    TK_OR,
-    TK_NOT,
     TK_LT,
     TK_LE,
-    TK_EQ,
     TK_GT,
     TK_GE,
-    TK_NE,
-    EPSILON,
     TK_ERROR,
-    DOLLAR,
-    SYN
-} TokenName;
+    EPSILON,
+    DOLLAR
+} token_type;
 
-typedef union
-{
+//int and float enough?
+typedef union {
     int num;
     float r_num;
-} Value;
+} value;
 
-typedef struct token
-{
-    TokenName name;
-    char *lexeme;
-    Value *value;
-    bool isint;
-    int lineno;
-} Token;
+struct TwinBuffer {
+    char* active_buffer;
+    char* load_buffer;
+    int forward_ptr;
+    int begin_ptr;
+    bool buffer_loaded;
+};
+
+struct Token {
+    token_type name;
+    char* lexeme;
+    value value;
+    bool value_is_int;
+    int line_num;
+};
 #endif
