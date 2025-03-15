bd4475ec5cd456b0cf6e8d791aef166caf4cd9ad
xvyxkth <avyakth03@gmail.com>
2025-03-15 20:08:53 +0530

    Mad shit

--- tester.c
+++ tester.c
@@ -1,3 +1,11 @@
+/*
+Group Number : 14
+Name : Avyakth Krishna Kumar   ID : 2021B3A71111P
+Name : Suchit Chebolu          ID : 2021B1A72281P
+Name : Abhiram H               ID : 2021B4A71134P
+Name : Ankur Renduchintala     ID : 2021B5A71159P
+Name : Vikram Hariharan        ID : 2022A7PS0013P
+*/
 #define _POSIX_C_SOURCE 200809L
 #define BUFFER_SIZE 1024
 
